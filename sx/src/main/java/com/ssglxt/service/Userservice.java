b24ba8fb473c3daa298411f14f3bb4fe8e9b214e
sxw3905859 <83241001+sxw3905859@users.noreply.github.com>
2022-06-29 22:25:42 +0800

    Delete sx directory

--- sx/src/main/java/com/ssglxt/service/Userservice.java
+++
@@ -1,8 +0,0 @@
-package com.ssglxt.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.ssglxt.doman.Users;
-import org.springframework.stereotype.Service;
-
-public interface Userservice extends IService<Users> {
-}
